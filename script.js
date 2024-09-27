d2b72410234c76b08af2b51727f857fdd6aadd4d
MiguelCarachenskiNeto <116725585+MiguelCarachenskiNeto@users.noreply.github.com>
2024-09-27 12:33:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let nome = "Miguel";
+let sobrenome = "Carachenski";
+let idade = 16;
+
+console.log(
+  "Meu nome é " + nome + " " + sobrenome + " e minha idade é " + idade
+);
+
+let mensagem = "Meu nome é ${nome} de ${sobrenome} e tenho ${idade} anos";
+
+console.log(mensagem);
